2f15897368163cdf8119b30fe9e4b23fb46ffcb3
Matthew Boeh <m@mboeh.com>
2020-10-21 19:38:34 -0700

    More pico than pico

--- picorogue.c
+++ picorogue.c
@@ -17,6 +17,7 @@ typedef enum
     TILE_BLOCKED
 } tile;
 
+#define TOPFLOOR 10
 #define XMIN 0
 #define YMIN 0
 #define XMAX 15
@@ -40,7 +41,12 @@ typedef struct
     int turns;
     int px, py;
     floor f;
-    bool won;
+    enum
+    {
+        PLAYING,
+        QUIT,
+        WON
+    } state;
 } game;
 
 game G = {
@@ -106,6 +112,118 @@ void draw()
     }
 }
 
+typedef enum
+{
+    N,
+    E,
+    S,
+    W
+} dir;
+
+void movepc(dir d)
+{
+    int cx, cy;
+    G.turns++;
+
+    cx = G.px;
+    cy = G.py;
+    switch (d)
+    {
+    case N:
+        cy--;
+        break;
+    case W:
+        cx--;
+        break;
+    case S:
+        cy++;
+        break;
+    case E:
+        cx++;
+    }
+    if (BOUNDSOK(cx, cy) && G.f.tiles[XY(cx, cy)] == TILE_CLEAR)
+    {
+        // it's an OK place to stand
+        G.px = cx;
+        G.py = cy;
+    }
+}
+
+void climb()
+{
+    G.turns++;
+    if (G.px != G.f.sx || G.py != G.f.sy)
+    {
+        // this isn't a stairwell?
+        return;
+    }
+    if (G.f.n == TOPFLOOR)
+    {
+        // we got to the top!
+        G.state = WON;
+        return;
+    }
+    newfloor();
+}
+
+void turn()
+{
+    char cmd;
+
+    printf("FL:%d T:%d | COMMAND? ", G.f.n, G.turns);
+    fflush(stdout);
+    cmd = getchar();
+
+    switch (cmd)
+    {
+    case 'q':
+    case 'Q':
+        G.state = QUIT;
+        break;
+    case 'h':
+        movepc(W);
+        break;
+    case 'j':
+        movepc(S);
+        break;
+    case 'k':
+        movepc(N);
+        break;
+    case 'l':
+        movepc(E);
+        break;
+    case '<':
+        climb();
+        break;
+    }
+}
+
+void gameover()
+{
+    CLS();
+
+    printf("Farewell, adventurer!\n\n");
+    printf("You ");
+    if (G.state == WON)
+    {
+        printf("reached the top in");
+    }
+    else if (G.state == QUIT)
+    {
+        printf("gave up after");
+    }
+    else
+    {
+        printf("ascended to demigodhood in");
+    }
+    printf(" %d turn", G.turns);
+    if (G.turns != 1)
+    {
+        printf("s");
+    }
+    printf(".\n\nThanks for playing PICOROGUE.\n");
+}
+
 int main()
 {
     time_t t;
@@ -114,7 +232,13 @@ int main()
 
     newfloor();
 
-    draw();
+    while (G.state == PLAYING)
+    {
+        draw();
+        turn();
+    }
+
+    gameover();
 
     return 0;
 }
